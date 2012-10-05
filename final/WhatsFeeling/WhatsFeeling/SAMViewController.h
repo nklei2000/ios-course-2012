bc84ab2b7c9bd024fd0ed3437ee091c3ea0059b2
Nam Kin Lei <samlei@Nams-Mac-Pro.local>
2012-10-05 15:58:08 +0800

    Init Project

---
+++ final/WhatsFeeling/WhatsFeeling/SAMViewController.h
@@ -0,0 +1,13 @@
+//
+//  SAMViewController.h
+//  WhatsFeeling
+//
+//  Created by Nam Kin Lei on 10/5/12.
+//  Copyright (c) 2012 Nam Kin Lei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAMViewController : UIViewController
+
+@end
