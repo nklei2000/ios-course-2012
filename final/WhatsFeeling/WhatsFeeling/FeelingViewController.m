421800866be85d41c6575211db30495fae5c8b86
Nam Kin Lei <samlei@Nams-Mac-Pro.local>
2012-10-08 18:25:41 +0800

    commitaaaa

--- final/WhatsFeeling/WhatsFeeling/FeelingViewController.m
+++ final/WhatsFeeling/WhatsFeeling/FeelingViewController.m
@@ -51,83 +51,83 @@
 #pragma mark -
 #pragma mark Table view methods
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [messages count];
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    static NSString *CellIdentifier = @"Cell";
-	
-	UIImageView *balloonView;
-	UILabel *label;
-	
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil)
-    {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
-		cell.selectionStyle = UITableViewCellSelectionStyleNone;
-		tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-		
-		balloonView = [[UIImageView alloc] initWithFrame:CGRectZero];
-		balloonView.tag = 1;
-		
-		label = [[UILabel alloc] initWithFrame:CGRectZero];
-		label.backgroundColor = [UIColor clearColor];
-		label.tag = 2;
-		label.numberOfLines = 0;
-		label.lineBreakMode = UILineBreakModeWordWrap;
-		label.font = [UIFont systemFontOfSize:14.0];
-		
-		UIView *message = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, cell.frame.size.width, cell.frame.size.height)];
-		message.tag = 0;
-		[message addSubview:balloonView];
-		[message addSubview:label];
-		[cell.contentView addSubview:message];
-		
-//		[balloonView release];
-//		[label release];
-//		[message release];
-	}
-	else
-	{
-		balloonView = (UIImageView *)[[cell.contentView viewWithTag:0] viewWithTag:1];
-		label = (UILabel *)[[cell.contentView viewWithTag:0] viewWithTag:2];
-	}
-	
-	NSString *text = [messages objectAtIndex:indexPath.row];
-	CGSize size = [text sizeWithFont:[UIFont systemFontOfSize:14.0] constrainedToSize:CGSizeMake(240.0f, 480.0f) lineBreakMode:UILineBreakModeWordWrap];
-	
-	UIImage *balloon;
-	
-	if(indexPath.row % 2 == 0)
-	{
-		balloonView.frame = CGRectMake(320.0f - (size.width + 28.0f), 2.0f, size.width + 28.0f, size.height + 15.0f);
-		balloon = [[UIImage imageNamed:@"green.png"] stretchableImageWithLeftCapWidth:24 topCapHeight:15];
-		label.frame = CGRectMake(307.0f - (size.width + 5.0f), 8.0f, size.width + 5.0f, size.height);
-	}
-	else
-	{
-		balloonView.frame = CGRectMake(0.0, 2.0, size.width + 28, size.height + 15);
-		balloon = [[UIImage imageNamed:@"grey.png"] stretchableImageWithLeftCapWidth:24 topCapHeight:15];
-		label.frame = CGRectMake(16, 8, size.width + 5, size.height);
-	}
-	
-	balloonView.image = balloon;
-	label.text = text;
-	
-    return cell;
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-	NSString *body = [messages objectAtIndex:indexPath.row];
-	CGSize size = [body sizeWithFont:[UIFont systemFontOfSize:14.0] constrainedToSize:CGSizeMake(240.0, 480.0) lineBreakMode:UILineBreakModeWordWrap];
-	return size.height + 15;
-}
+//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+//    return 1;
+//}
+//
+//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+//    return [messages count];
+//}
+//
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+//    
+//    static NSString *CellIdentifier = @"Cell";
+//	
+//	UIImageView *balloonView;
+//	UILabel *label;
+//	
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+//    if (cell == nil)
+//    {
+//        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+//		cell.selectionStyle = UITableViewCellSelectionStyleNone;
+//		tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+//		
+//		balloonView = [[UIImageView alloc] initWithFrame:CGRectZero];
+//		balloonView.tag = 1;
+//		
+//		label = [[UILabel alloc] initWithFrame:CGRectZero];
+//		label.backgroundColor = [UIColor clearColor];
+//		label.tag = 2;
+//		label.numberOfLines = 0;
+//		label.lineBreakMode = UILineBreakModeWordWrap;
+//		label.font = [UIFont systemFontOfSize:14.0];
+//		
+//		UIView *message = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, cell.frame.size.width, cell.frame.size.height)];
+//		message.tag = 0;
+//		[message addSubview:balloonView];
+//		[message addSubview:label];
+//		[cell.contentView addSubview:message];
+//		
+////		[balloonView release];
+////		[label release];
+////		[message release];
+//	}
+//	else
+//	{
+//		balloonView = (UIImageView *)[[cell.contentView viewWithTag:0] viewWithTag:1];
+//		label = (UILabel *)[[cell.contentView viewWithTag:0] viewWithTag:2];
+//	}
+//	
+//	NSString *text = [messages objectAtIndex:indexPath.row];
+//	CGSize size = [text sizeWithFont:[UIFont systemFontOfSize:14.0] constrainedToSize:CGSizeMake(240.0f, 480.0f) lineBreakMode:UILineBreakModeWordWrap];
+//	
+//	UIImage *balloon;
+//	
+//	if(indexPath.row % 2 == 0)
+//	{
+//		balloonView.frame = CGRectMake(320.0f - (size.width + 28.0f), 2.0f, size.width + 28.0f, size.height + 15.0f);
+//		balloon = [[UIImage imageNamed:@"green.png"] stretchableImageWithLeftCapWidth:24 topCapHeight:15];
+//		label.frame = CGRectMake(307.0f - (size.width + 5.0f), 8.0f, size.width + 5.0f, size.height);
+//	}
+//	else
+//	{
+//		balloonView.frame = CGRectMake(0.0, 2.0, size.width + 28, size.height + 15);
+//		balloon = [[UIImage imageNamed:@"grey.png"] stretchableImageWithLeftCapWidth:24 topCapHeight:15];
+//		label.frame = CGRectMake(16, 8, size.width + 5, size.height);
+//	}
+//	
+//	balloonView.image = balloon;
+//	label.text = text;
+//	
+//    return cell;
+//}
+//
+//- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+//	NSString *body = [messages objectAtIndex:indexPath.row];
+//	CGSize size = [body sizeWithFont:[UIFont systemFontOfSize:14.0] constrainedToSize:CGSizeMake(240.0, 480.0) lineBreakMode:UILineBreakModeWordWrap];
+//	return size.height + 15;
+//}
 
 
 @end
