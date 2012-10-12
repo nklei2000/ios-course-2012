ce0ec2b2c5f02e691c4b2eb18907c81c9c48c1e8
Nam Kin Lei <samlei@Nams-Mac-Pro.local>
2012-10-12 19:14:42 +0800

    sss

--- final/WhatsFeeling/WhatsFeeling/LoginViewController.m
+++ final/WhatsFeeling/WhatsFeeling/LoginViewController.m
@@ -202,6 +202,9 @@
                  {
                      NSLog(@"username: %@", [userInfo objectForKey:@"username"]);
                  }
+                 
+                 [self userDidSignIn];
+                 
              }
              
          }
