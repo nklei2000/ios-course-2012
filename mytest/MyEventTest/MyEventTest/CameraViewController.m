0e2c0d9304ce21d08d85e2f0cf52d8eadff3222c
Nam Kin Lei <samlei@Nams-Mac-Pro.local>
2012-10-23 02:12:59 +0800

    sss

--- mytest/MyEventTest/MyEventTest/CameraViewController.m
+++ mytest/MyEventTest/MyEventTest/CameraViewController.m
@@ -170,4 +170,47 @@
 }
 */
 
+
+/*
+ - (void) snap: (NSTimer *) timer
+ {
+ UIImageOrientation imageOrientation = currentImageOrientation(helper.isUsingFrontCamera, NO);
+ 
+ ciImage = helper.ciImage;
+ UIImage *baseImage = [UIImage imageWithCIImage:ciImage orientation:imageOrientation];
+ CGRect imageRect = (CGRect){.size = baseImage.size};
+ 
+ NSDictionary *detectorOptions = [NSDictionary dictionaryWithObject:CIDetectorAccuracyLow forKey:CIDetectorAccuracy];
+ 
+ CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeFace context:nil options:detectorOptions];
+ 
+ ExifOrientation detectOrientation = detectorEXIF(helper.isUsingFrontCamera, NO);
+ NSLog(@"Current orientation: %@", exifOrientationNameFromOrientation(detectOrientation));
+ 
+ NSDictionary *imageOptions = [NSDictionary dictionaryWithObject:[NSNumber numberWithInt:detectOrientation] forKey:CIDetectorImageOrientation];
+ NSArray *features = [detector featuresInImage:ciImage options:imageOptions];
+ if (!features.count) return;
+ CIFaceFeature *feature = [features lastObject]; // only one at a time
+ 
+ CGRect rect = rectInEXIF(detectOrientation, feature.bounds, imageRect);
+ if (deviceIsPortrait() && helper.isUsingFrontCamera) // workaround
+ {
+ rect.origin = CGPointFlipHorizontal(rect.origin, imageRect);
+ rect.origin = CGPointOffset(rect.origin, -rect.size.width, 0.0f);
+ }
+ 
+ // Expand by about 10%
+ CGPoint center = CGRectGetCenter(rect);
+ CGFloat width = rect.size.width * 1.1f;
+ CGFloat height = rect.size.height * 1.1f;
+ CGRect newRect = CGRectAroundCenter(center, width / 2.0f, height / 2.0f);
+ 
+ UIImage *newImage = [baseImage subImageWithBounds:newRect];
+ imageView.image = newImage;
+ }
+
+ 
+ 
+*/
+
 @end
